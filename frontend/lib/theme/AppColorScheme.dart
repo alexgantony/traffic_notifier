f480dc207b69653c8ca31439fa216709f8b79b93
alexgantony <aleximu03@gmail.com>
2025-06-25 16:33:16 +0530

    feat: implement login screen with styling, theming, and password toggle

---
+++ frontend/lib/theme/AppColorScheme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFFD1EDFF);
+  static const card = Color(0xFFFFFFFF);
+  static const heading = Color(0xFF2C3E91);
+  static const label = Color(0xFF000000);
+  static const toggleActive = Color(0xFF4C4CFF);
+  static const toggleTrack = Color(0xFFD1EDFF);
+}
